6d838e71167ff9e4a0773895426ff4c5a965777f
nihilok <mjfullstack@gmail.com>
2022-01-29 17:45:15 +0000

    improve-setup

--- setup.zsh
+++ setup.zsh
@@ -2,14 +2,24 @@
 
 ZSH=$ZSH || ~/.oh-my-zsh
 
+FILEDATE=date +%F
+FILE=~/.zshrc.bak.$FILEDATE
 
-mv ~/.zshrc ~/.zshrc-pre-jarv || exit
+if [ -f "$FILE" ]; then
+	echo "$FILE exists."
+	mkdir -p ~/.backup
+	mv $FILE ~/.backup
+fi
+
+cp ~/.zshrc ~/.zshrc.bak.$FILEDATE
 
 ln ./zsh/.zshrc ~/.zshrc
 
 ln ./zsh/themes/kali-simple.zsh-theme $ZSH/themes/kali-simple.zsh-theme
 
 ln .aliases $ZSH/custom/aliases 
+
+touch $ZSH/custom/my.aliases
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
